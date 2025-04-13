28c96e8b7d75a76d9c016d1671bdce94ef40cb13
eukd <dkejdlcl123@naver.com>
2025-04-13 17:32:23 +0900

    feat: 기업정보입력페이지1 생성 및 라우팅 수정

--- src/common/router.ts
+++ src/common/router.ts
@@ -1,20 +1,35 @@
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Layout from "@/ui/layout/Layout.tsx";
 import LandingPage from "@/ui/pages/landingPage/LandingPage.tsx";
-import {sampleLoader} from "@/services/SampleService.ts";
+import { sampleLoader } from "@/services/SampleService.ts";
+import CompanyInputPage1 from "@/ui/pages/companyInputPage/companyInputPage1/CompanyInputPage1";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
+  {
+    path: "/",
+    children: [
+      {
+        index: true,
+        Component: LandingPage,
+      },
+
+      {
+        path: "",
         Component: Layout,
         children: [
-            {
-                path: "/home",
-                loader: sampleLoader,
-                Component: LandingPage
-            }
-        ]
-    }
-])
+          {
+            path: "home",
+            loader: sampleLoader,
+            Component: LandingPage,
+          },
+          {
+            path: "company-info/step1",
+            Component: CompanyInputPage1,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
-export default router
+export default router;
---
+++ src/ui/pages/companyInputPage/companyInputPage1/CompanyInputPage1.tsx
@@ -0,0 +1,13 @@
+import { Typography, Stack } from "@mui/material";
+
+const CompanyInputPage1 = () => {
+  return (
+    <Stack sx={{ mt: 10 }}>
+      <Typography variant="subtitle1" fontWeight="bold">
+        기업정보입력 첫번째 페이지입니다.
+      </Typography>
+    </Stack>
+  );
+};
+
+export default CompanyInputPage1;
