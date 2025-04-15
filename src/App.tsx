1c8b88329a75fc51fefdacc049b65158f3874ef2
eukd <dkejdlcl123@naver.com>
2025-04-15 21:50:37 +0900

    chore: CssBaseline 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import router from "@/common/router.ts";
 import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import theme from "@/ui/theme";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <RouterProvider router={router} />
     </ThemeProvider>
   );
