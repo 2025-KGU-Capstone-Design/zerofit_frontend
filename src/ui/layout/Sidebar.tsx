f0f2a179cbca1d231d60dc04d15b47c7fbd7022a
eukd <dkejdlcl123@naver.com>
2025-04-16 23:09:42 +0900

    style: 사이드바 아이콘, 배경색 추가 및 아이콘, 텍스트 컬러 설정

--- src/ui/layout/Sidebar.tsx
+++ src/ui/layout/Sidebar.tsx
@@ -4,17 +4,28 @@ import {
   Toolbar,
   List,
   ListItem,
+  ListItemIcon,
   ListItemText,
 } from "@mui/material";
 import { Link } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
+import ApartmentIcon from "@mui/icons-material/Apartment";
+import QueryStatsIcon from "@mui/icons-material/QueryStats";
 
 const drawerWidth = 240;
 
 const navItems = [
-  { label: "Home", to: "/home" },
-  { label: "기업 정보 입력", to: "/company-info/step1" },
-  { label: "AI 탄소 감축 솔루션", to: "/solution" },
-  { label: "AI 최적 솔루션 제안", to: "/optimal-solution" },
+  { label: "Home", to: "/home", icon: <HomeIcon /> },
+  {
+    label: "기업 정보 입력",
+    to: "/company-info/step1",
+    icon: <ApartmentIcon />,
+  },
+  {
+    label: "AI 탄소 감축 솔루션 분석",
+    to: "/solution",
+    icon: <QueryStatsIcon />,
+  },
 ];
 
 const Sidebar = () => {
@@ -27,15 +38,40 @@ const Sidebar = () => {
         "& .MuiDrawer-paper": {
           width: drawerWidth,
           boxSizing: "border-box",
+          backgroundColor: (theme) => theme.palette.primary.main,
         },
       }}
     >
-      <Toolbar />
+      <Toolbar sx={{ mt: 1 }} />
       <Box sx={{ overflow: "auto" }}>
         <List>
           {navItems.map((item) => (
-            <ListItem key={item.to} component={Link} to={item.to}>
-              <ListItemText primary={item.label} />
+            <ListItem
+              key={item.to}
+              component={Link}
+              to={item.to}
+              sx={{ mb: 1 }}
+            >
+              {item.icon && (
+                <ListItemIcon
+                  sx={{
+                    minWidth: 32,
+                    color: "white",
+                  }}
+                >
+                  {item.icon}
+                </ListItemIcon>
+              )}
+              <ListItemText
+                primary={item.label}
+                slotProps={{
+                  primary: {
+                    sx: {
+                      color: "white",
+                    },
+                  },
+                }}
+              />
             </ListItem>
           ))}
         </List>
