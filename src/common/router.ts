6cdb896987f144e5bbebed11815644c1a47e9f0a
sunkiteo-o <hyhy020301@gmail.com>
2025-04-24 20:36:00 +0900

    feat: 솔루션 추천 페이지 컴포넌트트생성

--- src/common/router.ts
+++ src/common/router.ts
@@ -5,6 +5,7 @@ import {sampleLoader} from '@/services/SampleService.ts'
 import CompanyInputPage1 from '@/ui/pages/companyInputPage/companyInputPage1/CompanyInputPage1'
 import CompanyInputPage2 from '@/ui/pages/companyInputPage/companyInputPage2/CompanyInputPage2'
 import CompanyInputPage3 from '@/ui/pages/companyInputPage/companyInputPage3/CompanyInputPage3'
+import SolutionRecommend from '@/ui/pages/solutionRecommendPage/SolutionRecommend'
 
 const router = createBrowserRouter([
     {
@@ -27,14 +28,6 @@ const router = createBrowserRouter([
                     {
                         path: 'company-info/step1',
                         Component: CompanyInputPage1,
-                    },
-                    {
-                        path: 'company-info/step2',
-                        Component: CompanyInputPage2,
-                    },
-                    {
-                        path: 'company-info/step3',
-                        Component: CompanyInputPage3,
                     },
                 ],
             },
