efed1619070352664eb4236e2c1808ca14c041ea
sunkiteo-o <hyhy020301@gmail.com>
2025-05-08 18:47:47 +0900

    feat: 솔루션 내부 Icon 추가

--- src/ui/pages/solutionRecommendPage/common/Solution.tsx
+++ src/ui/pages/solutionRecommendPage/common/Solution.tsx
@@ -2,6 +2,11 @@ import {Box, Button, Stack, Typography} from '@mui/material'
 import Ranking from './Ranking'
 import Bookmark from './Bookmark'
 
+import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled'
+import TrendingUpIcon from '@mui/icons-material/TrendingUp'
+import EnergySavingsLeafIcon from '@mui/icons-material/EnergySavingsLeaf'
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn'
+
 const Solution = () => {
     return (
         <Box
@@ -34,33 +39,70 @@ const Solution = () => {
                     <Typography sx={{fontSize: '16px', color: '#4B5563'}}>
                         예상 절감액
                     </Typography>
-                    <Typography sx={{fontSize: '16px', fontWeight: 700}}>
-                        연간 5억 원
-                    </Typography>
+
+                    <Stack direction={'row'} sx={{alignItems: 'center'}}>
+                        <TrendingUpIcon
+                            sx={{
+                                width: 20,
+                                height: 20,
+                                marginRight: '8px',
+                                color: '#22C55E',
+                            }}
+                        />
+                        <Typography sx={{fontSize: '16px', fontWeight: 700}}>
+                            연간 5억 원
+                        </Typography>
+                    </Stack>
                 </Box>
                 <Box sx={{display: 'center', justifyContent: 'space-between'}}>
                     <Typography sx={{fontSize: '16px', color: '#4B5563'}}>
                         투자 회수 기간
                     </Typography>
-                    <Typography sx={{fontSize: '16px', fontWeight: 700}}>
-                        2.5년 내 회수
-                    </Typography>
+                    <Stack direction={'row'} sx={{alignItems: 'center'}}>
+                        <AccessTimeFilledIcon
+                            sx={{width: 20, height: 20, marginRight: '8px'}}
+                        />
+                        <Typography sx={{fontSize: '16px', fontWeight: 700}}>
+                            2.5년 내 회수
+                        </Typography>
+                    </Stack>
                 </Box>
                 <Box sx={{display: 'center', justifyContent: 'space-between'}}>
                     <Typography sx={{fontSize: '16px', color: '#4B5563'}}>
                         연간 온실가스 감축량
                     </Typography>
-                    <Typography sx={{fontSize: '16px', fontWeight: 700}}>
-                        200 tCO₂
-                    </Typography>
+
+                    <Stack direction={'row'} sx={{alignItems: 'center'}}>
+                        <EnergySavingsLeafIcon
+                            sx={{
+                                width: 20,
+                                height: 20,
+                                marginRight: '8px',
+                                color: '#22C55E',
+                            }}
+                        />
+                        <Typography sx={{fontSize: '16px', fontWeight: 700}}>
+                            200 tCO₂
+                        </Typography>
+                    </Stack>
                 </Box>
                 <Box sx={{display: 'center', justifyContent: 'space-between'}}>
                     <Typography sx={{fontSize: '16px', color: '#4B5563'}}>
                         투자 비용
                     </Typography>
-                    <Typography sx={{fontSize: '16px', fontWeight: 700}}>
-                        3억 원
-                    </Typography>
+                    <Stack direction={'row'} sx={{alignItems: 'center'}}>
+                        <MonetizationOnIcon
+                            sx={{
+                                width: 20,
+                                height: 20,
+                                marginRight: '8px',
+                                color: '#EAB308',
+                            }}
+                        />
+                        <Typography sx={{fontSize: '16px', fontWeight: 700}}>
+                            3억 원
+                        </Typography>
+                    </Stack>
                 </Box>
             </Stack>
             <Box
