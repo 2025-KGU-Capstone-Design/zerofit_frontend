aecb29d0f7e18fd3b6835b7ce33fe9ad0a2e5996
sunkiteo-o <hyhy020301@gmail.com>
2025-05-16 21:41:54 +0900

    feat: 솔루션 타입 추가

---
+++ src/store/useSolutionStore.ts
@@ -0,0 +1,31 @@
+import {create} from 'zustand'
+import {SolutionGroup} from '@/types/solution'
+import {persist, createJSONStorage} from 'zustand/middleware'
+
+type SolutionStore = {
+    solutions: SolutionGroup //저장할 상태 데이터
+    setAllSolutions: (data: SolutionGroup) => void //전체 솔루션 데이터 한 번에 업데이트
+    resetSolutions: () => void //솔루션 데이터를 빈 상태로 초기화
+}
+
+const initialSolutions: SolutionGroup = {
+    total_optimization: [],
+    emission_reduction: [],
+    cost_saving: [],
+    roi: [],
+}
+const useSolutionStore = create<SolutionStore>()(
+    persist(
+        (set) => ({
+            solutions: initialSolutions,
+            setAllSolutions: (data) => set({solutions: data}),
+            resetSolutions: () => set({solutions: initialSolutions}),
+        }),
+        {
+            name: 'solution-storage', // localStorage key 이름
+            storage: createJSONStorage(() => localStorage),
+        }
+    )
+)
+
+export default useSolutionStore
