ebc431ed06d8bbf82f4d6509c6d0ab9c2aa285f0
eukd <dkejdlcl123@naver.com>
2025-05-20 17:44:38 +0900

    refactor: SampleLoader 라우팅 제거

--- src/common/router.ts
+++ src/common/router.ts
@@ -2,7 +2,6 @@ import {createBrowserRouter} from 'react-router-dom'
 import Layout from '@/ui/layout/Layout.tsx'
 import LandingPage from '@/ui/pages/landingPage/LandingPage.tsx'
 import LoginPage from '@/ui/pages/login/LoginPage'
-import {sampleLoader} from '@/services/SampleService.ts'
 import CompanyInputPage1 from '@/ui/pages/companyInputPage/companyInputPage1/CompanyInputPage1'
 import CompanyInputPage2 from '@/ui/pages/companyInputPage/companyInputPage2/CompanyInputPage2'
 import CompanyInputPage3 from '@/ui/pages/companyInputPage/companyInputPage3/CompanyInputPage3'
@@ -23,11 +22,6 @@ const router = createBrowserRouter([
                 path: '',
                 Component: Layout,
                 children: [
-                    {
-                        path: 'home',
-                        loader: sampleLoader,
-                        Component: LandingPage,
-                    },
                     {path: 'company-info/step1', Component: CompanyInputPage1},
                     {path: 'company-info/step2', Component: CompanyInputPage2},
                     {path: 'company-info/step3', Component: CompanyInputPage3},
