0b81d4025448408f22f064ea4bb42e0a9c14b02d
eukd <dkejdlcl123@naver.com>
2025-06-04 20:09:43 +0900

    feat: axios 인터셉터 추가하여 localStorage의 token을 Authorization 헤더에 자동 설정

--- src/services/Http.ts
+++ src/services/Http.ts
@@ -5,4 +5,20 @@ const http = axios.create({
     withCredentials: false,
 })
 
+http.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('access_token')
+
+        if (token) {
+            config.headers = config.headers ?? {}
+            config.headers.Authorization = `Bearer ${token}`
+        }
+
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
+    }
+)
+
 export default http
