e4c7e7edfd61e5ebbc1ed4b2143b7e7b9b7ae07f
sunkiteo-o <hyhy020301@gmail.com>
2025-06-10 06:56:14 +0900

    feat: zustand Solution store에 CommentCache 추가가

--- src/store/useSolutionStore.ts
+++ src/store/useSolutionStore.ts
@@ -1,16 +1,31 @@
 import {create} from 'zustand'
 import {persist, createJSONStorage} from 'zustand/middleware'
 import {SolutionGroup} from '@/types/solution'
+import {SolutionType} from '@/constants/solutionCategories'
+import {SolutionCommentResponse} from '@/types/solutionComment'
 
 type SolutionHistoryItem = {
     requestId: number
     solutions: SolutionGroup
 }
 
+type CommentsCacheType = {
+    [requestId: string]: {
+        [category in SolutionType]?: SolutionCommentResponse
+    }
+}
+
 type SolutionStore = {
     current: SolutionHistoryItem | null
     setCurrent: (item: SolutionHistoryItem) => void
     resetCurrent: () => void
+    commentsCache: CommentsCacheType
+    setComment: (
+        requestId: number,
+        category: SolutionType,
+        comment: SolutionCommentResponse
+    ) => void
+    resetCommentsCache: () => void
 }
 
 const useSolutionStore = create<SolutionStore>()(
@@ -19,6 +34,22 @@ const useSolutionStore = create<SolutionStore>()(
             current: null,
             setCurrent: (item) => set({current: item}),
             resetCurrent: () => set({current: null}),
+            commentsCache: {},
+            setComment: (requestId, category, comment) =>
+                set((state) => {
+                    const key = requestId.toString()
+                    return {
+                        commentsCache: {
+                            ...state.commentsCache,
+                            [key]: {
+                                ...(state.commentsCache[key] || {}),
+                                [category]: comment,
+                            },
+                        },
+                    }
+                }),
+
+            resetCommentsCache: () => set({commentsCache: {}}),
         }),
         {
             name: 'solution-storage',
