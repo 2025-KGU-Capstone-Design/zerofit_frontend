72bdf17614080107f0a6781be31795c89006d40a
최석영 <szero0713@gmail.com>
2025-08-20 18:14:22 +0900

    UI 일관성 유지
    - 로그인, 회원가입 화면.
    - 랜딩페이지 Fade 효과 추가.

--- src/ui/pages/landingPage/HeroSection.tsx
+++ src/ui/pages/landingPage/HeroSection.tsx
@@ -8,6 +8,7 @@ import {useAuth} from '@/hooks/useAuth'
 import {useSnackbar} from '@/ui/CommonSnackbar'
 import useCompanyInputStore from '@/store/useCompanyInputStore'
 import ShinyText from '@/ui/bits/ShinyText.tsx'
+import FadeContent from '@/ui/bits/FadeContent.tsx'
 
 const HeroSection = () => {
 
@@ -29,126 +30,127 @@ const HeroSection = () => {
     return <>
         <div style={{width: '100%', height: '100vh', position: 'relative'}}>
             <DarkVeil />
-            <Stack alignItems="center"
-                   textAlign="center"
-                   px={3}
-                   py={10}
-                   sx={{
-                       position: 'absolute',
-                       top: 0,
-                       left: 0,
-                       width: '100%',
-                       height: '100%',
-                       zIndex: 1,
-                   }}>
-                <Box sx={{display: 'flex', alignItems: 'center', mb: 3}}>
-                    <Box
-                        component="img"
-                        src={Logo}
-                        sx={{height: '5rem', mr: 0.3}}
-                    />
+            <FadeContent>
+                <Stack alignItems="center"
+                       textAlign="center"
+                       px={3}
+                       py={10}
+                       sx={{
+                           position: 'absolute',
+                           top: 0,
+                           left: 0,
+                           width: '100%',
+                           height: '100%',
+                           zIndex: 1,
+                       }}>
+                    <Box sx={{display: 'flex', alignItems: 'center', mb: 3}}>
+                        <Box
+                            component="img"
+                            src={Logo}
+                            sx={{height: '5rem', mr: 0.3}}
+                        />
+                        <Typography
+                            variant="h1"
+                            fontWeight="bold"
+                            color="white"
+                            sx={{
+                                fontSize: '5rem',
+                            }}
+                        >
+                            Zero
+                            {/*<ShinyText text="Zero" />*/}
+                        </Typography>
+                        <Typography
+                            variant="h1"
+                            fontWeight="bold"
+                            color="secondary.main"
+                            sx={{
+                                fontSize: '5rem',
+                            }}
+                        >
+                            Fit
+                        </Typography>
+                    </Box>
+
                     <Typography
-                        variant="h1"
-                        fontWeight="bold"
+                        variant="h2"
+                        fontWeight={600}
                         color="white"
                         sx={{
-                            fontSize: '5rem',
+                            fontSize: '3rem',
+                            mb: 1,
                         }}
                     >
-                        Zero
-                        {/*<ShinyText text="Zero" />*/}
+                        <TextType
+                            text={['당신의 기업에 딱 맞는 Net-Zero 솔루션', '탄소 중립을 향한 여정,', 'Zero-Fit과 함께 시작하세요!']}
+                            textColors={['#e0e0e0', '#e0e0e0', '#e0e0e0']}
+                            typingSpeed={75}
+                            pauseDuration={1500}
+                            showCursor={true}
+                            cursorCharacter="|"
+                        />
                     </Typography>
+
                     <Typography
-                        variant="h1"
-                        fontWeight="bold"
-                        color="secondary.main"
+                        variant="body1"
+                        color="grey.300"
                         sx={{
-                            fontSize: '5rem',
+                            fontSize: '1rem',
+                            mt: 1,
+                            mb: 2,
+                            maxWidth: '40rem',
                         }}
                     >
-                        Fit
+                        ZeroFit은 기업의 산업군, 설비, 예산을 반영해 최적의 온실가스 감축
+                        솔루션을 추천하고, 감축 효과를 분석해 탄소 중립(Net-Zero) 목표
+                        달성을 지원합니다.
                     </Typography>
-                </Box>
 
-                <Typography
-                    variant="h2"
-                    fontWeight={600}
-                    color="white"
-                    sx={{
-                        fontSize: '3rem',
-                        mb: 1,
-                    }}
-                >
-                    <TextType
-                        text={['당신의 기업에 딱 맞는 Net-Zero 솔루션', '탄소 중립을 향한 여정,', 'Zero-Fit과 함께 시작하세요!']}
-                        textColors={['#e0e0e0', '#e0e0e0', '#e0e0e0']}
-                        typingSpeed={75}
-                        pauseDuration={1500}
-                        showCursor={true}
-                        cursorCharacter="|"
-                    />
-                </Typography>
+                    <Typography
+                        variant="h4"
+                        fontWeight={600}
+                        sx={{
+                            fontSize: '2.25rem',
+                            mt: 1,
+                        }}
+                    >
+                        <Box component="span" color="secondary.main">
+                            Net-
+                        </Box>
+                        <Box component="span" color="white">
+                            Zero
+                        </Box>
+                        <Box component="span" color="secondary.main">
+                            , Let&apos;s{' '}
+                        </Box>
+                        <Box component="span" color="white">
+                            Zero
+                        </Box>
+                        <Box component="span" color="secondary.main">
+                            !
+                        </Box>
+                    </Typography>
 
-                <Typography
-                    variant="body1"
-                    color="grey.300"
-                    sx={{
-                        fontSize: '1rem',
-                        mt: 1,
-                        mb: 2,
-                        maxWidth: '40rem',
-                    }}
-                >
-                    ZeroFit은 기업의 산업군, 설비, 예산을 반영해 최적의 온실가스 감축
-                    솔루션을 추천하고, 감축 효과를 분석해 탄소 중립(Net-Zero) 목표
-                    달성을 지원합니다.
-                </Typography>
 
-                <Typography
-                    variant="h4"
-                    fontWeight={600}
-                    sx={{
-                        fontSize: '2.25rem',
-                        mt: 1,
-                    }}
-                >
-                    <Box component="span" color="secondary.main">
-                        Net-
+                    <Box
+                        sx={{
+                            mt: '3rem',
+                            width: '18rem',
+                            cursor: 'pointer',
+                        }}
+                        onClick={handleStartClick}
+                    >
+                        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(4, 217, 217, 0.3)">
+                            <Typography variant="body1" fontWeight={600} sx={{
+                                fontSize: '1.7rem',
+                            }}>
+                                {/*시작하기*/}
+                                <ShinyText text={'시작하기'} />
+                            </Typography>
+                        </SpotlightCard>
                     </Box>
-                    <Box component="span" color="white">
-                        Zero
-                    </Box>
-                    <Box component="span" color="secondary.main">
-                        , Let&apos;s{' '}
-                    </Box>
-                    <Box component="span" color="white">
-                        Zero
-                    </Box>
-                    <Box component="span" color="secondary.main">
-                        !
-                    </Box>
-                </Typography>
-
-
-                <Box
-                    sx={{
-                        mt: '3rem',
-                        width: '18rem',
-                        cursor: 'pointer',
-                    }}
-                    onClick={handleStartClick}
-                >
-                    <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(4, 217, 217, 0.3)">
-                        <Typography variant="body1" fontWeight={600} sx={{
-                            fontSize: '1.7rem',
-                        }}>
-                            {/*시작하기*/}
-                            <ShinyText text={"시작하기"}/>
-                        </Typography>
-                    </SpotlightCard>
-                </Box>
-
-            </Stack>
+                </Stack>
+            </FadeContent>
         </div>
     </>
 }
